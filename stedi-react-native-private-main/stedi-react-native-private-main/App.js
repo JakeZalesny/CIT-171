060609c62352911ea3fcfcce0e0891121a101506
JakeZalesny <jrzalesny@gmail.com>
2022-10-20 13:24:30 -0600

    Revert "several changes"
    This reverts commit 933dec8281974f0011cbccf4268dc2e564c43c5a.

--- stedi-react-native-private-main/stedi-react-native-private-main/App.js
+++ stedi-react-native-private-main/stedi-react-native-private-main/App.js
@@ -1,39 +1,47 @@
-import React, { useEffect, useState, } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, AsyncStorage } from 'react-native';
-import  Navigation from './components/Navigation';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import OnboardingScreen from './screens/OnboardingScreen';
-import Home from './screens/Home';
-import { NavigationContainer } from '@react-navigation/native';
-import { TextInput } from 'react-native-paper';
-
-
-
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  AsyncStorage,
+} from "react-native";
+import Navigation from "./components/Navigation";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import OnboardingScreen from "./screens/OnboardingScreen";
+import Home from "./screens/Home";
+import { NavigationContainer } from "@react-navigation/native";
+import { Button, TextInput } from "react-native-paper";
 
 const AppStack = createNativeStackNavigator();
 
-const App = () =>{
+const App = () => {
   const [isFirstLaunch, setFirstLaunch] = React.useState(true);
   const [isLoggedIn, setIsLoggedIn] = React.useState(false);
   const [homeTodayScore, setHomeTodayScore] = React.useState(0);
 
-   if (isFirstLaunch == true){
-return(
-  <OnboardingScreen setFirstLaunch={setFirstLaunch}/>
- 
-);
-   }else if(isLoggedIn) {
-    return <Navigation/>;
-  }else{
+  if (isFirstLaunch == true) {
+    return <OnboardingScreen setFirstLaunch={setFirstLaunch} />;
+  } else if (isLoggedIn) {
+    return <Navigation />;
+  } else {
     return (
       <View>
-        <TextInput 
-        style={styles.input} 
-        placeholderTextColor='#4251f5'
-        placeholder='Cell Phone'></TextInput>
+        <TextInput
+          style={styles.input}
+          placeholderTextColor="#4251f5"
+          placeholder="Cell Phone"
+        ></TextInput>
+        <Button
+          title="Send"
+          style={styles.Button}
+          onPress={() => {
+            console.log("Button was pressed");
+          }}
+        />
       </View>
-    )
+    );
   }
-}
- export default App;
-
+};
+export default App;
